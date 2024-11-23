1eb5270e257efa5a8f65a92d090a6550e05145e5
Szymon Piątek <119783254+SzymonPiatek@users.noreply.github.com>
2024-11-23 11:06:19 +0100

    Add infinite scroll menu with example site (#1)
    * Infinite menu - html, css/scss, js
    
    * infiniteMenuSections
    
    * Example navbar init
    
    * Random data for infiniteMenuSections
    
    * fix js
    
    * Fixes
    
    * sections
    
    * Slide in and hover
    
    * Fix slide in

---
+++ data/infiniteMenuSectionsData.js
@@ -0,0 +1,66 @@
+const infiniteMenuSectionsData = [
+    {
+        title: "Main Menu",
+        titleHref: "#mainMenu",
+        blocks: [
+            { name: "Block 1", href: "/" },
+            { name: "Very Extended Block 2", href: "/" },
+            { name: "Block 3", href: "/" },
+            { name: "Extended Block 4", href: "/" },
+            { name: "Very Extended Block 5", href: "/" },
+        ],
+    },
+    {
+        title: "Second Tile",
+        titleHref: "#secondTile",
+        blocks: [
+            { name: "Extended Block 1", href: "/" },
+        ],
+    },
+    {
+        title: "Example 3",
+        titleHref: "/",
+        blocks: [
+            { name: "Very Extended Block 1", href: "/" },
+            { name: "Extended Block 2", href: "/" },
+        ],
+    },
+    {
+        title: "About Us",
+        titleHref: "/",
+        blocks: [
+            { name: "Block 1", href: "/" },
+            { name: "Extended Block 2", href: "/" },
+            { name: "Block 3", href: "/"},
+            { name: "Block 4", href: "/" },
+        ],
+    },
+    {
+        title: "Contact Us",
+        titleHref: "/",
+        blocks: [
+            { name: "Block 1", href: "/" },
+            { name: "Block 2", href: "/" },
+            { name: "Block 3", href: "/" },
+            { name: "Block 4", href: "/" },
+        ],
+    },
+    {
+        title: "FAQ",
+        titleHref: "/",
+        blocks: [
+            { name: "Very Extended Block 1", href: "/" },
+        ],
+    },
+    {
+        title: "Example 7",
+        titleHref: "/",
+        blocks: [
+            { name: "Block 1", href: "/" },
+            { name: "Block 2", href: "/" },
+            { name: "Block 3", href: "/" },
+        ],
+    },
+];
+
+export default infiniteMenuSectionsData;
---
+++ js/generateInfiniteMenu.js
@@ -0,0 +1,42 @@
+import infiniteMenuSectionsData from "../data/infiniteMenuSectionsData.js";
+
+export function generateMenu(containerSelector) {
+    const container = document.querySelector(containerSelector);
+
+    if (container) {
+        container.innerHTML = infiniteMenuSectionsData.map((section) => `
+            <div class="infinite-menu--elements--element">
+                <a class="infinite-menu--elements--element--title" href="${section.titleHref}">
+                    ${section.title}
+                </a>
+                <div class="infinite-menu--elements--element--blocks">
+                    ${section.blocks.map((block) => `
+                        <a class="infinite-menu--elements--element--blocks--block" href="${block.href}">
+                            ${block.name}
+                        </a>`).join("")}
+                </div>
+            </div>`).join("");
+
+        const links = container.querySelectorAll("a");
+        links.forEach((link) => {
+            link.addEventListener("click", (event) => {
+                const href = link.getAttribute("href");
+
+                if (href.startsWith("#")) {
+                    event.preventDefault();
+                    const targetId = href.substring(1);
+                    const targetElement = document.getElementById(targetId);
+
+                    if (targetElement) {
+                        targetElement.scrollIntoView({
+                            behavior: "smooth",
+                            block: "start",
+                        });
+                    }
+                }
+            });
+        });
+    } else {
+        console.error(`Kontener ${containerSelector} nie został znaleziony.`);
+    }
+}
